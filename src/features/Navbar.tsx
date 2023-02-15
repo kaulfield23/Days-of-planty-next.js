3f313dcebe751ed42987fd69238d16835b6b0ad0
kaulfield23 <kaulfield23@gmail.com>
2023-02-15 22:10:46 +0100

    boiler plate

---
+++ src/features/Navbar.tsx
@@ -0,0 +1,14 @@
+import { AppBar } from '@mui/material';
+import Button from '@mui/material/Button';
+import Link from 'next/link';
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <>
+      <AppBar position="sticky" sx={{ backgroundColor: '#99b3a9' }}></AppBar>
+    </>
+  );
+};
+
+export default Navbar;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
+import Navbar from '@/features/Navbar';
 import '@/styles/globals.css'
+import { PageWithLayout } from '@/utils';
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  const {...restProps} = pageProps;
+  const c = Component as PageWithLayout;
+  const getLayout = c.getLayout || ((page) => <>{page}</>);
+
+  return (
+  <>
+    <Navbar/>
+    {getLayout(<Component {...restProps} />, restProps)}
+  </>
+  )
 }
