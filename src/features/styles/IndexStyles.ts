207cd602cd86d6f1e96cf7e15124c5fdb3b32e77
kaulfield23 <kaulfield23@gmail.com>
2023-03-12 22:49:16 +0100

    Change Navbar to a better one

---
+++ src/features/styles/IndexStyles.ts
@@ -0,0 +1,9 @@
+import { SxProps, Theme } from '@mui/material';
+
+export const indexStyle: Record<string, SxProps<Theme> | undefined> = {
+  backgroundStyle: {
+    backgroundColor: 'beige',
+    height: '100vh',
+    width: '100vw',
+  },
+};
