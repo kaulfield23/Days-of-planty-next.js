803bec4064c669c13fa882630a11419298a41403
kaulfield23 <kaulfield23@gmail.com>
2023-06-29 14:33:47 +0200

    add tabs for categorization

--- src/features/styles/plantPageStyles.ts
+++ src/features/styles/plantPageStyles.ts
@@ -23,4 +23,18 @@ export const plantPageStyle: Record<string, SxProps<Theme> | undefined> = {
     height: '400px',
     backgroundColor: '#78b47d',
   },
+  plantTabsBox: {
+    flexGrow: 1,
+    bgcolor: '#f5f5f5',
+    display: 'flex',
+    height: 800,
+    width: '80%',
+    minWidth: '300px',
+    maxWidth: '1500px',
+    margin: '0 auto',
+    mt: 3,
+  },
+  plantTab: {
+    fontSize: { xs: 14, sm: 60 },
+  },
 };
