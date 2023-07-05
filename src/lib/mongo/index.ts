7efeeb1acc8549754255a7c9f937bffc67bfbdd9
kaulfield23 <kaulfield23@gmail.com>
2023-07-05 10:04:57 +0200

    check if it fetches data from db

--- src/lib/mongo/index.ts
+++ src/lib/mongo/index.ts
@@ -1,11 +1,10 @@
 import { MongoClient } from 'mongodb';
 
 const url = process.env.DB_URL;
-const options = {};
 
 if (!url) throw new Error('Please add your MongoDB url to .env.local');
 
-let client = new MongoClient(url, options);
+let client = new MongoClient(url);
 
 let clientPromise: Promise<MongoClient>;
 
@@ -17,13 +16,13 @@ if (process.env.NODE_ENV === 'development') {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
   if (!globalWithMongo._mongoClientPromise) {
-    client = new MongoClient(url, options);
+    client = new MongoClient(url);
     globalWithMongo._mongoClientPromise = client.connect();
   }
   clientPromise = globalWithMongo._mongoClientPromise;
 } else {
   // In production mode, it's best to not use a global variable.
-  client = new MongoClient(url, options);
+  client = new MongoClient(url);
   clientPromise = client.connect();
 }
 
