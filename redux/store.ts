f079628c97aefa30b24f71a5d5824e86b2254d86
kaulfield23 <kaulfield23@gmail.com>
2024-01-03 16:51:25 +0100

    remove react-persist

--- redux/store.ts
+++ redux/store.ts
@@ -1,7 +1,5 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import plantsSlice from './feature/plantSlice';
-import { persistStore, persistReducer } from 'redux-persist';
-import createWebStorage from 'redux-persist/lib/storage/createWebStorage';
 
 const createNoopStorage = () => {
   return {
@@ -16,24 +14,13 @@ const createNoopStorage = () => {
     },
   };
 };
-const storage =
-  typeof window !== 'undefined'
-    ? createWebStorage('local')
-    : createNoopStorage();
-
-const persistConfig = {
-  key: 'root',
-  storage,
-};
 
 const rootReducer = combineReducers({
   plantsReducer: plantsSlice.reducer,
 });
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 export const store = configureStore({
-  reducer: persistedReducer,
+  reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
@@ -42,4 +29,3 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-export const persistor = persistStore(store);
